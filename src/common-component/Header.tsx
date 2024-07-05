94f07472c8b6cef0d506193e67e1dff93ab18c07
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-07-05 16:34:08 +0530

    update file

--- src/common-component/Header.tsx
+++ src/common-component/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../redux/hook";
 import { fetchHeder } from "../redux/SliceApi";
 import HeaderProp from "./HeaderProp";
@@ -11,6 +11,28 @@ const Header = () => {
   useEffect(() => {
     dispatch(fetchHeder());
   }, [dispatch]);
+
+  const navOpen = () => {
+    const slider = document.getElementById("slider");
+    const open = document.getElementById("navOpen");
+    const close = document.getElementById("navClose");
+    if (slider && open && close) {
+      slider.style.width = "270px";
+      open.style.display = "none";
+      close.style.display = "block";
+    }
+  };
+
+  const navClose = () => {
+    const slider = document.getElementById("slider");
+    const open = document.getElementById("navOpen");
+    const close = document.getElementById("navClose");
+    if (slider && open && close) {
+      slider.style.width = "0";
+      open.style.display = "block";
+      close.style.display = "none";
+    }
+  };
   return (
     <div className={Style.container}>
       <div className={Style.Content}>
@@ -38,6 +60,44 @@ const Header = () => {
             <input type="text" placeholder="search" />
             <i className="fa-solid fa-magnifying-glass"></i>
           </div>
+        </div>
+        <div className={Style.button}>
+          <h2 id="navOpen" onClick={navOpen}>
+            ☰
+          </h2>
+          <h3 id="navClose" onClick={navClose} style={{ display: "none" }}>
+            X
+          </h3>
+        </div>
+      </div>
+
+      <div className={Style.sidebar} id="slider">
+        <div className={Style.inputbox}>
+          <input type="text" placeholder="Search" />
+        </div>
+        <div className={Style.navbar}>
+          {data.headerData &&
+            data.headerData.map((item) => {
+              return (
+                <>
+                  <HeaderProp data={item} />
+                </>
+              );
+            })}
+        </div>
+        <div className={Style.sideIcon}>
+          <ul>
+            <li>
+              <a href="#/" className={Style.whatsapp}>
+                <i className="fa-brands fa-whatsapp"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#/" className={Style.envelope}>
+                <i className="fa-solid fa-envelope"></i>Inquire Now
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
